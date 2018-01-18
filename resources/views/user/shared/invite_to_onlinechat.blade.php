eecd132044f8b4ffc57c8a4f34bac43999e4f7c3
Bobby Gemong <snailbob@live.com>
2018-01-18 19:11:25 +0800

    test peer websocket

--- resources/views/user/shared/invite_to_onlinechat.blade.php
+++ resources/views/user/shared/invite_to_onlinechat.blade.php
@@ -1,32 +1,40 @@
 
 <script type="text/ng-template" id="inviteToChatModal.html">
     <div class="modal-header">
-        <p> <strong>Invite to Chat</strong></p>
+        <p>
+            <strong>Invite to Chat</strong> <br>
+            You can add upto 3 more people to chat.
+
+        </p>
+        
 
         <button type="button" class="close" ng-click="cancel()" aria-label="Close"><span aria-hidden="true">&times;</span></button>
     </div>
-    <div class="modal-body group-chat-contacts" id="modal-body">
-        <div class="list-group">
-            <a class="list-group-item" ng-class="{'active': user.selected}" ng-repeat="user in users" ng-click="selectUser(user, $index)" ng-if="items.activeIndex != $index">
+    <div class="modal-body" id="modal-body">
+        <div class="group-chat-contacts gclist-modal">
+            <div class="list-group">
+                <a class="list-group-item" ng-class="{'active': user.selected}" ng-repeat="user in users" ng-click="selectUser(user, $index)" ng-if="items.activeIndex != $index">
 
-                <div class="media">
-                    <div class="media-left">
-                        <img class="media-object img-circle img-thumbnail" ng-src="@{{user.photo}}" width="45" alt="image">
+                    <div class="media">
+                        <div class="media-left">
+                            <img class="media-object img-circle img-thumbnail" ng-src="@{{user.photo}}" width="45" alt="image">
+                        </div>
+                        <div class="media-body">   
+
+                            <h4 class="media-heading">
+
+                                <i class="fa fa-circle fa-fw" ng-class="{'text-muted' : !user.is_online, 'text-success' : user.is_online}" aria-hidden="true"></i> @{{user.firstName}}
+                            </h4>
+
+                        </div>
                     </div>
-                    <div class="media-body">   
 
-                        <h4 class="media-heading">
+                </a>
+            </div>
 
-                            <i class="fa fa-circle fa-fw" ng-class="{'text-muted' : !user.is_online, 'text-success' : user.is_online}" aria-hidden="true"></i> @{{user.firstName}}
-                        </h4>
-
-                    </div>
-                </div>
-
-            </a>
         </div>
 
-        <div class="form-group text-right">
+        <div class="form-group text-right padding-top-15">
             <button class="btn btn-danger" ng-click="submit()">
                 Submit
             </button>
