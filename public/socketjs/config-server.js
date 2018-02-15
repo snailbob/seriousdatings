a413f76eb81b4f64d8ddf68659a23dd1cb833268
Mark Gocela <alecogkram@gmail.com>
2018-02-15 14:35:44 +0800

    ignored

--- public/socketjs/config-server.js
+++ public/socketjs/config-server.js
@@ -1,3 +1,10 @@
+
+    /*AUTHOR MARK
+        IN LOCAL : http://localhost:8888/  signaler
+        IN PROD SERVER : https://www.seriousdatings.com:8888/  signaler
+
+    */
+
 function getUUID() {
         return Math.round(Math.random() * 999999999) + 9995000;
     }
